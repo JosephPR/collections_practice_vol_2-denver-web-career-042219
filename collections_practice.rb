95b25c96d7f94540e0fe6b2c52a833eb6a1ae33f
Joseph Reilly <reilly4181@gmail.com>
2019-04-18 21:48:52 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,20 +1,15 @@
 def begins_with_r(arr)
   arr.all?{ 
-    
     |element| element.start_with?("r")
-  
 }
-
 end
 
 def contain_a (arr)
   arr2 = []
- arr.map{ |element|if element.include?("a")
+ arr.map{
+   |element|if element.include?("a")
  arr2.push(element)
-
  end
- 
  }
  arr2
-
 end
