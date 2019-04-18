7901dbbfa98fe9dfde99bba2e8034c53001d8457
Joseph Reilly <reilly4181@gmail.com>
2019-04-18 22:20:31 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -14,60 +14,38 @@ def contain_a (arr)
  arr2
 end
 
-
-def first_wa(array)
-  array.find do |el|
-    if el[0] == "w" && el[1] == "a"
-      el
-    end
+def first_wa(arr)
+    arr.detect do |string|
+    string[0,2] == "wa"
   end
 end
 
-
-def remove_non_strings(array)
-  array.delete_if do |el|
-    el.class != String
+ def remove_non_strings(arr)
+    arr.delete_if do |string| 
+    string.is_a?(String) == false
   end
-  array
 end
-
-
+  # binding.pry  
 def count_elements(array)
-  array.group_by(&:itself).map do |key, value|
-     key.merge({:count => value.length})
-  end
-end
 
+     array.uniq.each {|i| count = 0
+        array.each {|i2| if i2 == i then count += 1 end}
+        i[:count] = count}
+end
 
 
  def merge_data(keys, data)
   merged = []
-  keys.each {|i| data.first.map {|k,v|
-  if i.values[0] == k then merged << i.merge(v) end}}
+  keys.each {|i| data.first.map {|k,v| if i.values[0] == k then merged << i.merge(v) end}}
   merged
 end
 
-
-def find_cool(cool)
-  if cool[1].values[1] == "cool"
-    [cool[1]]
-  end
+ def find_cool(cool)
+    cool.select {|i| i.any? {|k,v| v == "cool"}} 
 end
 
-
-def organize_schools(schools)
-  array = []
-  schools.values.to_a.each do |value|
-    array << value.values
-  end
-  school_array = array.flatten.uniq.sort
-
-  h1 = {schools.to_a[0][1].values[0] => [schools.to_a[0][0], schools.to_a[1][0], schools.to_a[4][0]]}
-  h2 = {school_array[0] => [schools.to_a[3][0]]}
-  h3 = {school_array[2] => [schools.to_a[2][0], schools.to_a[5][0]]}
-  h = {}
-  h.merge!(h1)
-  h.merge!(h2)
-  h.merge!(h3)
-  h
+ def organize_schools(schools)
+    locations_hash = {}
+    schools.collect {|k,v| locations_hash[v[:location]] = []}
+    locations_hash.each {|k,v| schools.each {|k1,v1| if k == v1[:location] then v << k1  end}}
 end
